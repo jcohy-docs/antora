c049853b842ad6eb861af54e848d1b8185fc35b2
jiac <jiachao@xi-9.com>
2024-01-17 16:40:29 +0800

    first commit

---
+++ src/main/java/com/antora/antora/AntoraApplication.java
@@ -0,0 +1,13 @@
+package com.antora.antora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antora/antora/AntoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antora.antora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
